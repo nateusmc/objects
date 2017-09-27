44383766e348a176fec011c3f5f4280b65c05a70
Adam Bradbury <f.adambrad@gmail.com>
2017-09-27 13:03:03 -0600

    Object drills 2

--- objectdrills2.js
+++ objectdrills2.js
@@ -1,196 +1,419 @@
 'use strict';
-//1
+// //1
 
-// function makeStudentsReport(data) {
-//   return data.map(d => `${d.name}: ${d.grade}`);
+// // function makeStudentsReport(data) {
+// //   return data.map(d => `${d.name}: ${d.grade}`);
+// // }
+
+// // /* From here down, you are not expected to 
+// //    understand.... for now :)  
+   
+// //    Nothing to see here!
+   
+// // */
+
+
+// // // tests
+
+// // function testIt() {
+
+// //   const testData = [
+// //     {name: 'Jane Doe', grade: 'A'},
+// //     {name: 'John Dough', grade: 'B'},
+// //     {name: 'Jill Do', grade: 'A'}
+// //   ];
+
+// //   const expectations = [
+// //     'Jane Doe: A',
+// //     'John Dough: B',
+// //     'Jill Do: A'
+// //   ];
+
+// //   const results = makeStudentsReport(testData);
+
+// //   if (!(results && results instanceof Array)) {
+// //     console.error(
+// //       'FAILURE: `makeStudentsReport` must return an array');
+// //     return
+// //   }
+// //   if (results.length !== testData.length) {
+// //     console.error(
+// //       'FAILURE: test data had length of ' + testData.length +
+// //       ' but `makeStudentsReport` returned array of length ' + results.length);
+// //     return
+// //   }
+// //   for (let i=0; i < expectations.length; i++) {
+// //     let expect = expectations[i];
+// //     if (!results.find(function(item) {
+// //       return item === expect;
+// //     })) {
+// //       console.error(
+// //         'FAILURE: `makeStudentsReport` is not ' +
+// //         'producing expected strings'
+// //       );
+// //       return
+// //     }
+// //   }
+// //   console.log('SUCCESS: `makeStudentsReport` is working');
+// // }
+
+// // testIt();
+
+
+// //2
+
+// // const studentData = [
+// //   {
+// //     name: 'Tim',
+// //     status: 'Current student',
+// //     course: 'Biology'
+// //   },
+// //   {
+// //     name: 'Sue',
+// //     status: 'Withdrawn',
+// //     course: 'Mathematics'
+// //   },
+// //   {
+// //     name: 'Liz',
+// //     status: 'On leave',
+// //     course: 'Computer science'
+// //   }
+// // ];
+
+// // function enrollInSummerSchool(students) {
+// //   return students.map(student => {
+// //     return {
+// //       name: student.name,
+// //       status: 'In Summer school',
+// //       course: student.course
+// //     };
+// //   });
+// // }
+
+
+
+// // /* From here down, you are not expected to understand.... for now :)
+// //   Nothing to see here!
+
+// // */
+
+// // // tests
+
+
+// // function testIt() {
+// //   var testData = [
+// //     {
+// //       name: 'Burt',
+// //       status: 'Playing hooky',
+// //       course: 'Biology'
+// //     },
+// //     {
+// //       name: 'Melanie',
+// //       status: 'Sick',
+// //       course: 'Mathematics'
+// //     },
+// //     {
+// //       name: 'Leonard',
+// //       status: 'AWOL',
+// //       course: 'Computer science'
+// //     }
+// //   ];
+
+// //   var results = enrollInSummerSchool(testData);
+
+// //   if (!(results && results instanceof Array)) {
+// //     console.error('FAILURE: `enrollSummerSchool` must return an array');
+// //     return
+// //   }
+// //   var result = testData.every(function(student) {
+// //     var match = results.find(function(_student) {
+// //       return (
+// //         _student.name === student.name &&
+// //                     _student.course === student.course &&
+// //                     _student.status.toLowerCase() === 'in summer school'
+// //       );
+// //     }
+// //     );
+// //     return match !== undefined;
+// //   }
+// //   );
+// //   if (!result) {
+// //     console.error(
+// //       'FAILURE: `enrollSummerSchool` should return ' +
+// //             'original key/value pairs for each student, and ' +
+// //             'update `status` to "In Summer school": ' + JSON.stringify(results));
+// //   } else {
+// //     console.info('SUCCESS: `enrollSummerSchool` is working');
+// //   }
+// // }
+
+// // testIt();
+
+
+
+// //3
+
+// // you can pass in `scratchData` to test out `findByid`
+// // your function
+// const scratchData = [
+//   {id: 22, foo: 'bar'},
+//   {id: 28, foo: 'bizz'},
+//   {id: 19, foo: 'bazz'}
+// ];
+
+// function findById(items, idNum) {
+//   return items.find(item => item.id === idNum );
 // }
+
+
+// // 
+
+// function testIt() {
+//   const testData = [
+//     {id: 1, foo: 'bar'},
+//     {id: 2, foo: 'bizz'},
+//     {id: 3, bang: 'boo'}
+//   ];
+//   const result = findById(testData, 3);
+//   if (!(result && result !== null && typeof result === 'object')) {
+//     console.error('`findById` must return an object');
+//     return
+//   }
+//   if (result.id !== 3) {
+//     console.error(
+//       'Asked for item with id of `3` but got back one with id of ' +
+//       result.id
+//     );
+//     return
+//   }
+//   if (result.bang !== 'boo') {
+//     console.error('Expected all key/value pairs from target object to be returned');
+//     return
+//   }
+//   console.log('SUCCESS: `findByid` is working');
+// }
+
+// testIt();function validateKeys(object, expectedKeys) {  
+ 
+
+
+
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+// const objectA = {
+//   id: 2,
+//   name: 'Jane Doe',
+//   age: 34,
+//   city: 'Chicago'
+// };
+
+// // running the function with `objectB` and `expectedKeys`
+// // should return `false`
+// const objectB = {
+//   id: 3,
+//   age: 33,
+//   city: 'Peoria'
+// };
+
+// const expectedKeys = [
+//   'id', 'name', 'age', 'city'
+// ];
+
+// function validateKeys(object, expectedKeys){
+//   if (Object.keys(object)  ) { 
+//     object === expectedKeys;
+//     return true;
+//   }
+//   else {
+//     return false;
+//   }
+// }
+
+// // validateKeys(objectB, expectedKeys);
+
 
 // /* From here down, you are not expected to 
 //    understand.... for now :)  
+   
    
 //    Nothing to see here!
    
 // */
 
 
-// // tests
 
 // function testIt() {
+//   const objectA = {
+//     id: 2,
+//     name: 'Jane Doe',
+//     age: 34,
+//     city: 'Chicago'
+//   };
 
-//   const testData = [
-//     {name: 'Jane Doe', grade: 'A'},
-//     {name: 'John Dough', grade: 'B'},
-//     {name: 'Jill Do', grade: 'A'}
+//   const objectB = {
+//     id: 3,
+//     age: 33,
+//     city: 'Peoria'
+//   };
+
+//   const objectC = {
+//     id: 9,
+//     name: 'Billy Bear',
+//     age: 62,
+//     city: 'Milwaukee',
+//     status: 'paused'
+//   };
+
+//   const expectedKeys = [
+//     'id', 'name', 'age', 'city'
 //   ];
 
-//   const expectations = [
-//     'Jane Doe: A',
-//     'John Dough: B',
-//     'Jill Do: A'
-//   ];
-
-//   const results = makeStudentsReport(testData);
-
-//   if (!(results && results instanceof Array)) {
+//   if (typeof validateKeys(objectA, expectedKeys) !== 'boolean') {
 //     console.error(
-//       'FAILURE: `makeStudentsReport` must return an array');
-//     return
+//       'FAILURE: `validateKeys` should return a boolean value');
+//     return;
 //   }
-//   if (results.length !== testData.length) {
+
+
+//   if (!validateKeys(objectA, expectedKeys)) {
 //     console.error(
-//       'FAILURE: test data had length of ' + testData.length +
-//       ' but `makeStudentsReport` returned array of length ' + results.length);
-//     return
+//       'FAILURE: running `validateKeys` with the following object and keys ' +
+//       'should return `true` but returned `false`:\n' +
+//       objectA  + '\n' + expectedKeys
+//     );
+//     return;
 //   }
-//   for (let i=0; i < expectations.length; i++) {
-//     let expect = expectations[i];
-//     if (!results.find(function(item) {
-//       return item === expect;
-//     })) {
-//       console.error(
-//         'FAILURE: `makeStudentsReport` is not ' +
-//         'producing expected strings'
-//       );
-//       return
-//     }
+
+//   if (validateKeys(objectB, expectedKeys)) {
+//     console.error(
+//       'FAILURE: running `validateKeys` with the following object and keys ' +
+//       'should return `false` but returned `true`:\n' +
+//       objectB  + '\n' + expectedKeys
+//     );
 //   }
-//   console.log('SUCCESS: `makeStudentsReport` is working');
+
+//   if (validateKeys(objectC, expectedKeys)) {
+//     console.error(
+//       'FAILURE: running `validateKeys` with the following object and keys ' +
+//       'should return `false` but returned `true`:\n' +
+//       objectC  + '\n' + expectedKeys
+//     );
+//   }
+  
+//   console.log('SUCCESS: `validateKeys` is working');
 // }
 
 // testIt();
 
+// Validate Keys answer
 
-//2
+// const objectA = {
+//   id: 2,
+//   name: 'Jane Doe',
+//   age: 34,
+//   city: 'Chicago'
+// }
 
-// const studentData = [
-//   {
-//     name: 'Tim',
-//     status: 'Current student',
-//     course: 'Biology'
-//   },
-//   {
-//     name: 'Sue',
-//     status: 'Withdrawn',
-//     course: 'Mathematics'
-//   },
-//   {
-//     name: 'Liz',
-//     status: 'On leave',
-//     course: 'Computer science'
-//   }
+// const objectB = {
+//   id: 3,
+//   age: 33,
+//   city: 'Peoria'
+// }
+
+// const objectC = {
+//   id: 9,
+//   name: 'Billy Bear',
+//   age: 62,
+//   city: 'Milwaukee',
+//   status: 'paused'
+// }
+
+
+// const expectedKeys = [
+//   'id', 'name', 'age', 'city'
 // ];
 
-// function enrollInSummerSchool(students) {
-//   return students.map(student => {
-//     return {
-//       name: student.name,
-//       status: 'In Summer school',
-//       course: student.course
-//     };
-//   });
+// function validateKeys(object, expectedKeys) {
+  
+//   // if there's not the same number of object keys
+//   // and expected keys, then we know there are missing or
+//   // extra keys, so return false
+//   if (Object.keys(object).length !== expectedKeys.length) {
+//     return false;
+//   }
+  
+//   // we iterate over each expected key and verify that
+//   // it's found in `object`.
+//   for (let i; i<expectedKeys.length; i++) {
+//     if (Object.keys(object).find(key => key !== expectedKeys[i]
+//     )) {
+//       return false;
+//     }
+//   }
+//   // if we get to this point in our code, the keys are valid
+//   // so we return `true`
+//   return true;
 // }
-
-
-
-// /* From here down, you are not expected to understand.... for now :)
-//   Nothing to see here!
-
-// */
-
-// // tests
-
 
 // function testIt() {
-//   var testData = [
-//     {
-//       name: 'Burt',
-//       status: 'Playing hooky',
-//       course: 'Biology'
-//     },
-//     {
-//       name: 'Melanie',
-//       status: 'Sick',
-//       course: 'Mathematics'
-//     },
-//     {
-//       name: 'Leonard',
-//       status: 'AWOL',
-//       course: 'Computer science'
-//     }
+//   const objectA = {
+//     id: 2,
+//     name: 'Jane Doe',
+//     age: 34,
+//     city: 'Chicago'
+//   }
+
+//   const objectB = {
+//     id: 3,
+//     age: 33,
+//     city: 'Peoria'
+//   }
+
+//   const objectC = {
+//     id: 9,
+//     name: 'Billy Bear',
+//     age: 62,
+//     city: 'Milwaukee',
+//     status: 'paused'
+//   }
+
+//   const expectedKeys = [
+//     'id', 'name', 'age', 'city'
 //   ];
 
-//   var results = enrollInSummerSchool(testData);
-
-//   if (!(results && results instanceof Array)) {
-//     console.error('FAILURE: `enrollSummerSchool` must return an array');
-//     return
-//   }
-//   var result = testData.every(function(student) {
-//     var match = results.find(function(_student) {
-//       return (
-//         _student.name === student.name &&
-//                     _student.course === student.course &&
-//                     _student.status.toLowerCase() === 'in summer school'
-//       );
-//     }
-//     );
-//     return match !== undefined;
-//   }
-//   );
-//   if (!result) {
+//   if (typeof validateKeys(objectA, expectedKeys) !== 'boolean') {
 //     console.error(
-//       'FAILURE: `enrollSummerSchool` should return ' +
-//             'original key/value pairs for each student, and ' +
-//             'update `status` to "In Summer school": ' + JSON.stringify(results));
-//   } else {
-//     console.info('SUCCESS: `enrollSummerSchool` is working');
+//       'FAILURE: `validateKeys` should return a boolean value');
+//     return;
 //   }
+
+
+//   if (!validateKeys(objectA, expectedKeys)) {
+//     console.error(
+//       'FAILURE: running `validateKeys` with the following object and keys ' +
+//       'should return `true` but returned `false`:\n' +
+//       objectA  + '\n' + expectedKeys
+//     )
+//     return;
+//   }
+
+//   if (validateKeys(objectB, expectedKeys)) {
+//    console.error(
+//       'FAILURE: running `validateKeys` with the following object and keys ' +
+//       'should return `false` but returned `true`:\n' +
+//       objectB  + '\n' + expectedKeys
+//     );
+//   }
+
+//   if (validateKeys(objectC, expectedKeys)) {
+//    console.error(
+//       'FAILURE: running `validateKeys` with the following object and keys ' +
+//       'should return `false` but returned `true`:\n' +
+//       objectC  + '\n' + expectedKeys
+//     );
+//   }
+  
+//   console.log('SUCCESS: `validateKeys` is working');
 // }
 
-// testIt();
-
-
-
-//3
-
-// you can pass in `scratchData` to test out `findByid`
-// your function
-const scratchData = [
-  {id: 22, foo: 'bar'},
-  {id: 28, foo: 'bizz'},
-  {id: 19, foo: 'bazz'}
-];
-
-function findById(items, idNum) {
-  return items.find(item => item.id === idNum );
-}
-
-
-// 
-
-function testIt() {
-  const testData = [
-    {id: 1, foo: 'bar'},
-    {id: 2, foo: 'bizz'},
-    {id: 3, bang: 'boo'}
-  ];
-  const result = findById(testData, 3);
-  if (!(result && result !== null && typeof result === 'object')) {
-    console.error('`findById` must return an object');
-    return
-  }
-  if (result.id !== 3) {
-    console.error(
-      'Asked for item with id of `3` but got back one with id of ' +
-      result.id
-    );
-    return
-  }
-  if (result.bang !== 'boo') {
-    console.error('Expected all key/value pairs from target object to be returned');
-    return
-  }
-  console.log('SUCCESS: `findByid` is working');
-}
-
-testIt();
+// testIt()
